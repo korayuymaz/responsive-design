5ebe436f11ebfed205d87f855fccf93a55057e44
Koray <koray.uymaz@agesa.com.tr>
2025-01-29 20:47:01 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={reactLogo} alt="React Logo" />
+        <img src={viteLogo} alt="Vite Logo" />
+      </header>
+      <main>
+        <h1>Responsive Design</h1>
+        <p>
+          This is a simple example of a responsive design using CSS media
+          queries.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
